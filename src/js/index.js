c93d0dd0d8ac96ca63bedc6fa983fb4e8882e091
Jennifer Magpantay <jennifer.magpantay@gmail.com>
2022-06-27 15:52:16 +0200

    chore: Finished responsiveness for accessible navigation and mobile menu

--- src/js/index.js
+++ src/js/index.js
@@ -21,7 +21,7 @@ function handleClickSearchBar() {
 
 function handleClickMobileMenu() {
   const buttonMenu = document.querySelector("#button-menu-js");
-  buttonMenu.addEventListener("click", manageMenu);
+  buttonMenu.addEventListener("click", manageMenuMobile);
 }
 
 function expandSearchBar() {
@@ -51,10 +51,80 @@ function expandSearchBar() {
   }
 }
 
-function manageMenu() {
-  console.log("managing mobile menu");
-  // declare variables
-  // once button is clicked, display the navbar-menu
-  // when it is clicked again, hide the navbar-menu
-  // also make changes to aria-expanded, for screen readers
+function manageMenuMobile() {
+  const buttonMenu = document.querySelector("#button-menu-js");
+  const navbar = document.querySelector("#mobile--navigation-js");
+  const isNavbarVisible = navbar.getAttribute("data-visible");
+  const list = document.querySelectorAll("#mobile--navigation-js ul li a");
+
+  if (isNavbarVisible === "false") {
+    console.log(isNavbarVisible);
+    buttonMenu.setAttribute("aria-expanded", true);
+    buttonMenu.setAttribute("aria-label", "Close menu");
+    navbar.setAttribute("data-visible", true);
+    changeNodeListAttribute(list, "tabindex", "0");
+
+    // closing menu options
+    // once navbar is expanded (has data-visible set as true), allow it to be closed when:
+    // user clicks on a link or outside the navbar
+    closeMenuByClickOutsideNav(buttonMenu, list, navbar);
+
+    // user press esc to close navbar
+    closeMenuByEscapeKey(buttonMenu, list, navbar);
+
+    // nav loses focus
+    closeMenuWhenLosingFocus(buttonMenu, list, navbar);
+  } else {
+    console.log(isNavbarVisible);
+    // reset the values when navbar is closed
+    resetMobileMenu(buttonMenu, list, navbar);
+  }
+}
+
+function changeNodeListAttribute(list, attribute, value) {
+  list.forEach((item) => item.setAttribute(attribute, value));
+}
+
+function closeMenuByClickOutsideNav(button, list, nav) {
+  const img = document.querySelector("#icon-mobile-menu-js");
+  const isVisible = nav.getAttribute("data-visible");
+
+  if (isVisible === "true") {
+    window.addEventListener("click", (event) => {
+      if (
+        event.target === img ||
+        event.target === nav ||
+        event.target === button
+      ) {
+        return null;
+      } else {
+        resetMobileMenu(button, list, nav);
+      }
+    });
+  }
+}
+
+function closeMenuByEscapeKey(button, list, nav) {
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      resetMobileMenu(button, list, nav);
+    }
+  });
+}
+
+function closeMenuWhenLosingFocus(button, list, nav) {
+  // instead of keep track of blur or focusout => triggers run in 'delay'
+  // use focusin => once the event happens, it will reset the menu
+  window.addEventListener("focusin", (event) => {
+    if (event.target.className !== "menu--link-item") {
+      resetMobileMenu(button, list, nav);
+    }
+  });
+}
+
+function resetMobileMenu(button, list, nav) {
+  button.setAttribute("aria-expanded", false);
+  button.setAttribute("aria-label", "Open menu");
+  nav.setAttribute("data-visible", false);
+  changeNodeListAttribute(list, "tabindex", "-1");
 }
